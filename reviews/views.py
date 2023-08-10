1554d923b1626cba95f7929f8158f73670c2c19c
daniel <danielnobre09@gmail.com>
2023-08-10 18:08:13 -0300

    templateview

--- reviews/views.py
+++ reviews/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from .forms import ReviewForm
 from django.views import View
+from django.views.generic.base import TemplateView
+from .models import Review
 
 
 # Create your views here.
@@ -42,5 +44,35 @@ class ReviewView(View):
 #     })
 #
 
-def thank_you(request):
-    return render(request, "reviews/thank_you.html")
+class ThankYouView(TemplateView):
+    template_name = "reviews/thank_you.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['message'] = 'This works!'
+        return context
+
+# def thank_you(request):
+#     return render(request, "reviews/thank_you.html")
+
+
+class ReviewsListView(TemplateView):
+    template_name = "reviews/review_list.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        reviews = Review.objects.all()
+        context['reviews'] = reviews
+        return context
+
+
+class SingleReviewView(TemplateView):
+    template_name = "reviews/single_review.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        review_id = kwargs['id']
+        selected_review = Review.objects.get(pk=review_id)
+        context['review'] = selected_review
+        return context
+
