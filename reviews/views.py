3574bdf55f09c8256bb379ad698d2033f51f20d8
daniel <danielnobre09@gmail.com>
2023-08-10 18:32:25 -0300

    form view

--- reviews/views.py
+++ reviews/views.py
@@ -4,30 +4,39 @@ from .forms import ReviewForm
 from django.views import View
 from django.views.generic.base import TemplateView
 from .models import Review
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import FormView
 
 
 # Create your views here.
 
 
-class ReviewView(View):
-    def get(self, request):
-        form = ReviewForm()
+class ReviewView(FormView):
+    form_class = ReviewForm
+    template_name = "reviews/review.html"
+    success_url = "/thank-you"
 
-        return render(request, "reviews/review.html", {
-            "form": form
-        })
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
 
-    def post(self, request):
-        form = ReviewForm(request.POST)
+    # def get(self, request):
+    #     form = ReviewForm()
+    #
+    #     return render(request, "reviews/review.html", {
+    #         "form": form
+    #     })
 
-        if form.is_valid():
-            form.save()
-            return HttpResponseRedirect("/thank-you")
-
-        return render(request, "reviews/review.html", {
-            "form": form
-        })
+    # def post(self, request):
+    #     form = ReviewForm(request.POST)
+    #
+    #     if form.is_valid():
+    #         form.save()
+    #         return HttpResponseRedirect("/thank-you")
+    #
+    #     return render(request, "reviews/review.html", {
+    #         "form": form
+    #     })
 
 
 # def review(request):
@@ -68,13 +77,8 @@ class ReviewsListView(ListView):
     #     return data
 
 
-class SingleReviewView(TemplateView):
+class SingleReviewView(DetailView):
     template_name = "reviews/single_review.html"
+    model = Review
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        review_id = kwargs['id']
-        selected_review = Review.objects.get(pk=review_id)
-        context['review'] = selected_review
-        return context
 
